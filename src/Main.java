71612e68bdd30750813df0ced85092a34c5123e4
ibrahimbaltaci <ibaltaci001@gmail.com>
2022-09-23 23:33:19 +0300

    First 3 idea for basic issue. Measuring time for each method.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int num1=20;
-        int num2=25;
-        int num3=2;
-
+        int num1 = 20;
+        int num2 = 25;
+        int num3 = 2;
         long startTime = System.currentTimeMillis(); //for measuring performance of code//
-        //VERSION 1
         /*
+        //VERSION 1
         if (num1<num2){
             if (num2<num3){
                 System.out.println("Biggest Number is " + num3);
@@ -17,9 +16,7 @@ public class Main {
             System.out.println("Biggest Number is " + num3);
         }
         else System.out.println("Biggest Number is " + num1);
-*/
         //VERSION 2
-        /*
 
     boolean comp1 = num1<num2;
     boolean comp2 = num3<num2;
@@ -30,20 +27,26 @@ public class Main {
         System.out.println("Biggest Number is " + num3);
     }
     else System.out.println("Biggest Number is " + num1);
-        */
         //VERSION 3
 
-        int biggestNum =num1;
-if (biggestNum < num2){
-    biggestNum = num2;
-}
-if (biggestNum<num3){
-    biggestNum = num3;
-}
+        int biggestNum = num1;
+        if (biggestNum < num2) {
+            biggestNum = num2;
+        }
+        if (biggestNum < num3) {
+            biggestNum = num3;
+        }
         System.out.println("Biggest Number is " + biggestNum);
-
-    //for measuring performance of code
-
+*/
+        //for measuring performance of code
+        int[] numList = {num1, num2, num3}; //fastest way!!!
+        int biggest = numList[0];
+        for (int number : numList) {
+            if (biggest < number) {
+                biggest = number;
+            }
+        }
+        System.out.println(biggest);
         long endTime = System.currentTimeMillis();
         long duration = (endTime - startTime);
         System.out.println(duration + " ms");
